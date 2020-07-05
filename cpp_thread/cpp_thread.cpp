7482368c3bc7dd229b25d25d8d9645720ba3fd0c
colderleo <colderleo@163.com>
2020-07-05 20:14:44 +0800

    test

--- cpp_thread/cpp_thread.cpp
+++ cpp_thread/cpp_thread.cpp
@@ -20,60 +20,68 @@ public:
 	Foo() {}
 
 public:
-	void first(function<void()> printFirst) {
+	void first() {
 		// 等待直至 main() 发送数据
-		std::unique_lock<std::mutex> lk(g_mutex);
-		// printFirst() outputs "first". Do not change or remove this line.
-		printFirst();
+		//std::unique_lock<std::mutex> lk(g_mutex);
+		printf("print first.\n");
 		// 通知前完成手动解锁，以避免等待线程才被唤醒就阻塞（细节见 notify_one ）
 		counter++;
 		cv1.notify_one();
 	}
 
-	void second(function<void()> printSecond) {
+	void second() {
 		std::unique_lock<std::mutex> lk(g_mutex);
 		cv1.wait(lk, [this]() {return counter == 2; }); // 阻塞当前线程，直到条件变量被唤醒 
-		// printSecond() outputs "second". Do not change or remove this line.
-		printSecond();
+		printf("print second.\n");
 		counter++;
 		cv2.notify_one();
 	}
 
-	void third(function<void()> printThird) {
+	void third() {
 
 		std::unique_lock<std::mutex> lk(g_mutex);
 		cv2.wait(lk, [this]() {return counter == 3; });
-		// printThird() outputs "third". Do not change or remove this line.
-		printThird();
+		printf("print thrid.\n");
 	}
 
 };
 
+mutex mutex1;
 
-void printFirst() {
-	printf("print first.\n");
+void test1() {
+	unique_lock<mutex> lk(mutex1);
+	for (int i = 0; i < 5; ++i) {
+		this_thread::sleep_for(chrono::seconds(1));
+		printf("%d\n", i);
+	}
 }
 
-void printSecond() {
-	printf("print second.\n");
-}
-
-void printThird() {
-	printf("print thrid.\n");
+void test2() {
+	unique_lock<mutex> lk(mutex1);
+	
+	for (int i = 0; i < 5; ++i) {
+		this_thread::sleep_for(chrono::seconds(1));
+		printf("%d\n", i);
+	}
 }
 
 
 int main()
 {
-	string name;
-
-	Foo f;
-	thread t2(&Foo::second, &f, printSecond);
-	this_thread::sleep_for(chrono::seconds(3));
-	thread t1(&Foo::first, &f, printFirst);
-	thread t3(&Foo::third, &f, printThird);
-	t2.join();
+	thread t1(test1);
+	thread t2(test1);
 	t1.join();
-	t3.join();
+	t2.join();
+	return 0;
+
+	//Foo f;
+	//thread t3(&Foo::third, &f);
+	//this_thread::sleep_for(chrono::seconds(1));
+	//thread t2(&Foo::second, &f);
+	//this_thread::sleep_for(chrono::seconds(1));
+	//thread t1(&Foo::first, &f);
+	//t2.join();
+	//t1.join();
+	//t3.join();
 }
 
