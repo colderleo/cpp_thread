21e7a735b24bad732fabe4cbf7dfef9dfd03e9ad
cvster <535674963@qq.com>
2020-07-07 13:42:56 +0800

    change

--- cpp_thread/print_in_order.cpp
+++ cpp_thread/print_in_order.cpp
@@ -11,8 +11,7 @@ using namespace std;
 class Foo {
 private:
 	int counter = 1;
-	std::condition_variable cv1;
-	std::condition_variable cv2;
+	std::condition_variable cv1, cv2;
 	// 使用lock和unlock手动加锁
 	std::mutex g_mutex;
 
@@ -22,7 +21,7 @@ public:
 public:
 	void first() {
 		// 等待直至 main() 发送数据
-		//std::unique_lock<std::mutex> lk(g_mutex);
+		std::unique_lock<std::mutex> lk(g_mutex);  
 		printf("print first.\n");
 		// 通知前完成手动解锁，以避免等待线程才被唤醒就阻塞（细节见 notify_one ）
 		counter++;
@@ -38,7 +37,6 @@ public:
 	}
 
 	void third() {
-
 		std::unique_lock<std::mutex> lk(g_mutex);
 		cv2.wait(lk, [this]() {return counter == 3; });
 		printf("print thrid.\n");
@@ -46,42 +44,16 @@ public:
 
 };
 
-mutex mutex1;
 
-void test1() {
-	unique_lock<mutex> lk(mutex1);
-	for (int i = 0; i < 5; ++i) {
-		this_thread::sleep_for(chrono::seconds(1));
-		printf("%d\n", i);
-	}
-}
-
-void test2() {
-	unique_lock<mutex> lk(mutex1);
-	
-	for (int i = 0; i < 5; ++i) {
-		this_thread::sleep_for(chrono::seconds(1));
-		printf("%d\n", i);
-	}
-}
-
-
-int main()
+int rename_main()
 {
-	thread t1(test1);
-	thread t2(test1);
+	Foo f;
+	thread t2(&Foo::second, &f);
+	thread t3(&Foo::third, &f);
+	thread t1(&Foo::first, &f);
 	t1.join();
 	t2.join();
+	t3.join();
 	return 0;
-
-	//Foo f;
-	//thread t3(&Foo::third, &f);
-	//this_thread::sleep_for(chrono::seconds(1));
-	//thread t2(&Foo::second, &f);
-	//this_thread::sleep_for(chrono::seconds(1));
-	//thread t1(&Foo::first, &f);
-	//t2.join();
-	//t1.join();
-	//t3.join();
 }
 
